396746190fe5ece8aa798b5981b1c5652c133339
TimothePearce <timothe.pearce@gmail.com>
2021-12-30 11:09:10 +0100

    Add missing trailing-slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Laravel Quasar',
   tagline: 'Build Eloquent projections with ease.',
   url: 'https://timothepearce.github.io/',
-  baseUrl: '/laravel-quasar-docs',
+  baseUrl: '/laravel-quasar-docs/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
