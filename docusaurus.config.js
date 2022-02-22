981a1bba15cace955f8d268ed6aac61120d2d996
TimothePearce <timothe.pearce@gmail.com>
2022-02-22 14:23:40 +0100

    Update package name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  projectName: 'laravel-time-series.github.io',
+  projectName: 'laravel-time-series-doc.github.io',
   organizationName: 'TimothePearce',
   trailingSlash: false,
   deploymentBranch: 'gh-pages',
